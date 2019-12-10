62bd2be7fac0f620e8defee3025f570481ce0011
Nishan Ahmed <nishanahmad@gmail.com>
2019-12-10 15:17:42 +0530

    gitignore test

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
-$username = 'heuhjrbyzr'; 
-$password = 'F5mwAMg6xa';
-$database = 'heuhjrbyzr';
+$username = 'nishan'; 
+$password = 'darussalam123.';
+$database = 'fazleomarkoodali';
 
 
 $con = mysqli_connect($host, $username, $password, $database);
